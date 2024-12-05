5ed1860144bdf9c527f2046f286fea79e98830a6
Bruno Reis Torezan <98826121+btorezan@users.noreply.github.com>
2024-12-05 14:16:56 +0000

    feat(day-2): implementation of first part of challenge

---
+++ day-2/part-2.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+fs.readFile('input.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+
+  const linhas = data.split('\n');
+  let countTrue = 0;
+  
+   linhas.forEach(function(linha, i){
+     if(isReportSafe(linha)){
+        countTrue++;
+     }
+   });
+
+   console.log(countTrue);
+
+
+
+
+});
+
+function isReportSafe(report){
+    const numbers = report.split(" ");
+    let count = 0;
+    let diffs = [];
+    for(let i = 0; i<numbers.length-1; i++){
+        const diff = numbers[i]-numbers[i+1];
+    
+        if(Math.abs(diff)>3||diff==0) return false;
+       
+        diffs.push(diff);
+    }
+    if(!(isAllDiffsPositive(diffs)||isAllDiffsNegative(diffs))) return false;
+
+    return true;
+}
+
+
+
+function isAllDiffsPositive(diffs){
+    return diffs.every(numero => numero > 0);
+}
+
+function isAllDiffsNegative(diffs){
+    return diffs.every(numero => numero < 0);
+}
